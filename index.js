93da64682c62c95e1913c0e88ab8fb448a869793
Jason Botesazan <jbotesazan@gmail.com>
2021-12-05 14:26:36 -0500

    commit tree bd342f761ccba17e4ae7cdaa0c05229d94ffca7d

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name1, email1) {
+
+
+    const userData = {
+        name: name1,
+        email: email1,
+    }
+
+    const postData = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(userData)
+    }
+
+    const post = document.querySelector('body')
+    const div = document.createElement('div')
+    post.appendChild(div)
+
+    const fetchedData = fetch('http://localhost:3000/users', postData)
+        .then((res) => res.json())
+        .then((object) => {div.textContent = object.id})
+        .catch((error) => {
+            const errorMessage = document.createElement('div')
+            post.appendChild(errorMessage)
+            errorMessage.textContent = error.message;
+        });
+        return fetchedData;
+    }
